661cb3c838427527abda6ade3d419024dbb217bd
James Byess <james.byess@gmail.com>
2019-07-28 21:11:55 -0400

    enter loads new page

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-function init(e){
-
-	let search = document.getElementById('search')
+function init(e) {
+	let search = document.getElementById("search")
+	let form = document.querySelector('.search')
 	search.focus()
-	let links = Array.from(document.querySelectorAll('.link'))
-	let results = document.querySelector('.search-results')
+	search.value = ''
+	let links = Array.from(document.querySelectorAll(".link"))
+	let results = document.querySelector(".search-results")
 
-	search.addEventListener('keyup', (e) => {
-		results.innerHTML = ''
+	search.addEventListener("keyup", e => {
+		results.innerHTML = ""
 		let needle = new RegExp(`${e.target.value}`)
 		let matches = links.filter(link => needle.test(link.dataset.name))
 
 		matches.forEach(match => {
-			let matchItem = document.createElement('div')
+			let matchItem = document.createElement("div")
 			matchItem.appendChild(match)
 			results.appendChild(matchItem)
 		})
-		
 	})
 
+	form.addEventListener("submit", e => {
+		e.preventDefault()
+
+		let url = document.querySelector('.search-results a').href
+		console.log(url)
+		window.location = url
+	})
 }
 
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener("DOMContentLoaded", init)
